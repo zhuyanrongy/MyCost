fa79c7c0a0a561a5dcc023b018c067bfb9049fd6
zhuyanrongy <1172969906@qq.com>
2023-08-27 17:07:22 +0800

    引入vue - router

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "About",
+  setup(props) {
+    let msg = ref("我是About组件");
+    return {
+      msg,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "Home",
+  setup(props) {
+    let msg = ref("我是Home组件");
+    return {
+      msg,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
